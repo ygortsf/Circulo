bcfaf43e70136494cd05defc9bd2ba3e0d122f37
ygortsf <ygordetarcio@gmail.com>
2025-08-19 22:43:47 +0000

    commit terca_feira

--- CalcCirculo.java
+++ CalcCirculo.java
@@ -3,9 +3,23 @@ public class CalcCirculo {
 
         //Criando um objeto com Construtor
         Circulo c1 = new Circulo(3);  // O raio já é definido na criação do objeto
-        System.out.print("A área é: ");
-        System.out.println(c1.area());  // Método para calcular área
-        System.out.print("O comprimento é: ");
-        System.out.print(c1.comprimento());
+        //System.out.print("A área é: ");
+        //System.out.println(c1.area());  // Método para calcular área
+       // System.out.print("O comprimento é: ");
+       // System.out.print(c1.comprimento());
+
+       //       get e set na prática
+       
+       System.out.println("valor de raio: "+ c1.getRaio());
+       System.out.print("A área é: " + c1.area());
+       c1.setRaio(2);
+       System.out.println("valor novo de raio: "+ c1.getRaio());
+       System.out.print("A área é: "+ c1.area());
+
+
+
+
+        
+
     }
 }
--- Circulo.java
+++ Circulo.java
@@ -1,11 +1,33 @@
 public class Circulo{
 
- double raio;
+private double raio;
+
+// public void setRaio(int raio){     ----> metodo set de encapsulamento
+//       this.raio = raio;
+ // }
+
+
+ // public int getRaio(){        ----> metodo get de encapsulamento
+ //         return raio;
+//  }
 
     // Objeto com construtor
-    Circulo(int r){
+     Circulo(int r){
         raio = r;
     }
+
+    public void setRaio(double raio){
+        this.raio = raio;
+    }
+
+    public double getRaio(){
+        return this.raio;
+    }
+
+
+
+
+
 
     double area(){
         return 3.14 * raio * raio;
